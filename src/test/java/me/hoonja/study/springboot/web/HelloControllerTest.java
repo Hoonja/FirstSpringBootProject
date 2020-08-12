778b22b237bb3e67bdd0e3f3847f01c8ab68560a
hoonja <hoonja@gmail.com>
2020-08-12 18:21:19 +0900

    4장까지 완료

--- src/test/java/me/hoonja/study/springboot/web/HelloControllerTest.java
+++ src/test/java/me/hoonja/study/springboot/web/HelloControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,6 +17,7 @@ import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
