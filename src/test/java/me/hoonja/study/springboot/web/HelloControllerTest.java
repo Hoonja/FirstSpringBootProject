6ea6e6c6ff3e20c35a81e436009edf65516a320f
hoonja <hoonja@gmail.com>
2020-08-13 11:04:27 +0900

    HelloControllerTest가 실패하는 문제 해결

--- src/test/java/me/hoonja/study/springboot/web/HelloControllerTest.java
+++ src/test/java/me/hoonja/study/springboot/web/HelloControllerTest.java
@@ -3,8 +3,10 @@ package me.hoonja.study.springboot.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -17,7 +19,6 @@ import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
