c242a280fc1270c180c156c07e6158558b69c1d6
hoonja <hoonja@gmail.com>
2020-08-14 18:03:23 +0900

    5장까지 완료

---
+++ src/main/java/me/hoonja/study/springboot/config/auth/WebConfig.java
@@ -0,0 +1,23 @@
+package me.hoonja.study.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+//@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    public WebConfig(LoginUserArgumentResolver resolver) {
+        this.loginUserArgumentResolver = resolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/me/hoonja/study/springboot/web/IndexController.java
+++ src/main/java/me/hoonja/study/springboot/web/IndexController.java
@@ -2,6 +2,7 @@ package me.hoonja.study.springboot.web;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import me.hoonja.study.springboot.config.auth.LoginUser;
 import me.hoonja.study.springboot.config.auth.dto.SessionUser;
 import me.hoonja.study.springboot.service.posts.PostsService;
 import me.hoonja.study.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
